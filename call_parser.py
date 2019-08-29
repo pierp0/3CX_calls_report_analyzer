e40562eafbb202dcc37a26767834366717f08718
Pierpaolo Furiani <pierpaolo.furiani@gmail.com>
2019-08-29 18:20:14 +0200

    todo list in call_parser

--- call_parser.py
+++ call_parser.py
@@ -10,3 +10,16 @@ for file in fileList:
         actualFileConent = actualFile.getContent()
         for p in actualFileConent:
             print(p + '\n')#str(actualFileConent).split("\n"))
+
+# load conf file yaml
+# schedule every monday
+# 	Connect to email box
+#	download email and get link of the week
+#	dowload the  log file
+#	analize file
+#		number of total calls
+#		number of answered phone calls
+#		number of missed phone calls
+#		number of answered calls per operator (oper code)
+#	build a graphic of datas
+#	send an email to list-mail-address 
