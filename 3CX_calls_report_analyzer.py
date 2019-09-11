57581e3bdf694648f16fad6fcae535ba62cc58cf
Pierpaolo Furiani <pierpaolo.furiani@gmail.com>
2019-09-11 18:07:32 +0200

    added modality local file -l

--- 3CX_calls_report_analyzer.py
+++ 3CX_calls_report_analyzer.py
@@ -5,25 +5,48 @@ import sys
 import termcolor
 import time
 
-if __name__ == "__main__":
 
+def __getConfig(path='./'):
+    with open('./config.yml', 'r') as confFile:
+        conf = yaml.safe_load(confFile)
+    return conf
+
+
+if __name__ == "__main__":
+    conf = __getConfig()
     if len(sys.argv) == 1:
         print(termcolor.colored("""
         +----------------------------------------+
         |        STARTING 3CX ANALYZER           |
         +----------------------------------------+
         """, 'green'))
-
-        with open('./config.yml', 'r') as confFile:
-            conf = yaml.safe_load(confFile)
         while True:
-            if emailAndParser.downloadEmail(conf['download']['addr'], conf['download']['port'], conf['download']['usr'], conf['download']['pwd']):
-                print('QUI')
-                # lf = logfile.logfile()
+            path = emailAndParser.downloadEmail(conf['download']['addr'], conf['download']['port'], conf['download']['usr'], conf['download']['pwd'])
+            if path:
+                lf = logfile.logfile(path)
                 # emailAndParser.sendEmail(conf['send']['addr'], conf['send']['port'], conf['send']['usr'], conf['send']['pwd'], conf['send']['saddr'], conf['send']['raddr'])
                 break
             else:
                 time.sleep(15)
+    elif sys.argv[1] == '-l':
+        print(termcolor.colored("""
+        +----------------------------------------+
+        |        STARTING 3CX ANALYZER           |
+        +----------------------------------------+
+        """, 'green'))
+        fileList = os.listdir('./')
+        for file in fileList:
+            if '' in file:
+
+
+
+
+
+
+
+
+
+        lf = logfile.logfile()
 
     else:
         print(termcolor.colored("""
@@ -38,6 +61,7 @@ if __name__ == "__main__":
                 3CX_calls_report_analyzer.py [-h]
         DESCRIPTION
             -h  to show this help
+            -l  to upload report files from local directory
         MORE INFORMATION AT
             https://github.com/pierp0/3CX_calls_report_analyzer
         """)
--- emailAndParser.py
+++ emailAndParser.py
@@ -3,18 +3,15 @@ import smtplib
 import urllib.request
 # import base64
 import re
+import os
 
 
 def downloadFile(data):
-    print('\nDOWN\n')
     # https://tgs.my3cx.it:5001/management/Reports/LogChiamateSettimanale_0209_fiK9AQZJ4vvLaYAXITvb.csv
     test = re.search("https\:\/\/.{1,20}\.my3cx\.it\:5001\/management\/Reports\/.{1,30}_[0-9]{4}_[a-zA-Z0-9]{20}.csv", str(data)).group()
-    print('TEST\n')
-    print(test)
-    # linkIndex = data.find('here')
-    # link = ''
-    print('SAVE\n')
-    urllib.request.urlretrieve(test, 'newdata.txt')
+    nameFile = os.basename(test)
+    urllib.request.urlretrieve(test, nameFile)
+    return nameFile
 
 
 def sendEmail(srvaddr='', port=25, usr='', pwd='', saddr='', raddr=''):
@@ -30,27 +27,22 @@ def sendEmail(srvaddr='', port=25, usr='', pwd='', saddr='', raddr=''):
 
 def downloadEmail(srvaddr='', port='', usr='', pwd=''):
     try:
-        print('DOWNLOAD EMAIL')
-        # print('downloademail')
         email = imaplib.IMAP4_SSL(srvaddr, port)
-        # print('login')
         email.login(usr, pwd)
-        # print('select')
         email.select()
-
         typ, data = email.search(None, '(UNSEEN)')
-        print('prima di for')
+        
         for num in data[0].split():
             typ, data = email.fetch(num, '(RFC822)')
             print(data)
-            downloadFile(data)
+            path = downloadFile(data)
             # num1 = base64.b64decode(num)
             # data1 = base64.b64decode(data[0][1])
             # print('MESSAGE %s\n%s\n' % (num1, data1))
 
         email.close()
         email.logout()
-        return True
+        return path
     except Exception:
         print('ERRORE')
         return False
